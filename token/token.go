7d97d96e91055b8758da8f1b2272fc273c2658da
Shihab Mahamud <shihab4t@gmail.com>
2026-09-04 01:02:06 +0600

    Add operator/keyword tokens and fix lexer bugs
    Rename LPARAN/RPARAN to LPAREN/RPAREN, add remaining operator tokens
    (-, !, *, /, <, >) and keyword tokens (true, false, if, else, return),
    fix the false keyword mapping (was uppercase "FALSE"), and add a third
    lexer test covering the full token set.

--- token/token.go
+++ token/token.go
@@ -16,25 +16,43 @@ const (
 	INT   = "INT"   // 123456
 
 	// Operators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
 
 	// Delimiters
 	COMMA     = ","
 	SEMICOLON = ";"
 
-	LPARAN = "("
-	RPARAN = ")"
+	LPAREN = "("
+	RPAREN = ")"
 	LBRACE = "{"
 	RBRACE = "}"
 
+	// Keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
