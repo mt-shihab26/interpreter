e73a0446b5e8dc7e2e73ed829de3a4abbf2f2e4f
Shihab Mahamud <shihab4t@gmail.com>
2026-09-11 11:35:11 +0600

    split expressions

--- ast/ast.go
+++ ast/ast.go
@@ -6,40 +6,6 @@ import (
 	"strings"
 )
 
-type Node interface {
-	TokenLiteral() string
-	String() string
-}
-
-type Statement interface {
-	Node
-	statementNode()
-}
-
-type Expression interface {
-	Node
-	expressionNode()
-}
-
-type Program struct {
-	Statements []Statement
-}
-
-func (p *Program) TokenLiteral() string {
-	if len(p.Statements) > 0 {
-		return p.Statements[0].TokenLiteral()
-	}
-	return ""
-}
-
-func (p *Program) String() string {
-	var out bytes.Buffer
-	for _, s := range p.Statements {
-		out.WriteString(s.String())
-	}
-	return out.String()
-}
-
 type IdentifierExpression struct {
 	Token token.Token
 	Value string
@@ -133,95 +99,6 @@ func (be *BinaryExpression) String() string {
 	out.WriteString(" ")
 	out.WriteString(be.Right.String())
 	out.WriteString(")")
-	return out.String()
-}
-
-type LetStatement struct {
-	Token token.Token
-	Name  *IdentifierExpression
-	Value Expression
-}
-
-func (ls *LetStatement) statementNode() {
-
-}
-func (ls *LetStatement) TokenLiteral() string {
-	return ls.Token.Literal
-}
-
-func (ls *LetStatement) String() string {
-	var out bytes.Buffer
-	out.WriteString(ls.TokenLiteral())
-	out.WriteString(" ")
-	out.WriteString(ls.Name.String())
-	out.WriteString(" = ")
-	if ls.Value != nil {
-		out.WriteString(ls.Value.String())
-	}
-	out.WriteString(";")
-	return out.String()
-}
-
-type ReturnStatement struct {
-	Token       token.Token
-	ReturnValue Expression
-}
-
-func (rs *ReturnStatement) statementNode() {
-
-}
-func (rs *ReturnStatement) TokenLiteral() string {
-	return rs.Token.Literal
-}
-
-func (rs *ReturnStatement) String() string {
-	var out bytes.Buffer
-	out.WriteString(rs.TokenLiteral())
-	out.WriteString(" ")
-	if rs.ReturnValue != nil {
-		out.WriteString(rs.ReturnValue.String())
-	}
-	out.WriteString(";")
-	return out.String()
-}
-
-type ExpressionStatement struct {
-	Token      token.Token
-	Expression Expression
-}
-
-func (es *ExpressionStatement) statementNode() {
-
-}
-func (es *ExpressionStatement) TokenLiteral() string {
-	return es.Token.Literal
-}
-
-func (es *ExpressionStatement) String() string {
-	if es.Expression != nil {
-		return es.Expression.String()
-	}
-	return ""
-}
-
-type BlockStatement struct {
-	Token      token.Token
-	Statements []Statement
-}
-
-func (bs *BlockStatement) statementNode() {
-
-}
-
-func (bs *BlockStatement) TokenLiteral() string {
-	return bs.Token.Literal
-}
-
-func (bs *BlockStatement) String() string {
-	var out bytes.Buffer
-	for _, s := range bs.Statements {
-		out.WriteString(s.String())
-	}
 	return out.String()
 }
 
