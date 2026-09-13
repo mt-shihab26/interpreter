e140e959b4091ec14c1ec40e6f0746a0a13c7bb6
Shihab Mahamud <shihab4t@gmail.com>
2026-09-13 11:53:46 +0600

    wip

--- eval/eval_test.go
+++ eval/eval_test.go
@@ -176,6 +176,19 @@ func TestErrorObject(t *testing.T) {
 			`,
 			"identifier not found: j",
 		},
+		{
+			`
+			let printNum = fn(i) {
+				i;
+			}
+			printStr("10");
+			printNum(10);
+			let printStr = fn(s) {
+				s;
+			}
+			`,
+			"identifier not found: printStr",
+		},
 	}
 	for _, test := range tests {
 		program, evaluated := testEval(test.input)
@@ -245,19 +258,19 @@ func TestFunctionCalls(t *testing.T) {
 			`,
 			5,
 		},
-		// {
-		// 	`
-		// 	let printNum = fn(i) {
-		// 		i;
-		// 	}
-		// 	printStr("10");
-		// 	printNum(10);
-		// 	let printStr = fn(s) {
-		// 		s;
-		// 	}
-		// 	`,
-		// 	5,
-		// },
+		{
+			`
+			let printNum = fn(i) {
+				i;
+			}
+			printStr("10");
+			printNum(10);
+			let printStr = fn(s) {
+				s;
+			}
+			`,
+			5,
+		},
 	}
 	for _, test := range tests {
 		program, evaluated := testEval(test.input)
