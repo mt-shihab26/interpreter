ff670187c8b69cd21959e0d67e1b20de82d2466e
Shihab Mahamud <shihab4t@gmail.com>
2026-09-15 02:40:45 +0600

    execute source code

--- source/source.go
+++ source/source.go
@@ -1,12 +1,31 @@
 package source
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"strings"
+
+	"monkey/eval"
+	"monkey/lexer"
+	"monkey/object"
+	"monkey/parser"
 )
 
 func Run() error {
-	filename := os.Args[1]
-	fmt.Printf("%+v\n", filename)
+	fileName := os.Args[1]
+	content, err := os.ReadFile(fileName)
+	if err != nil {
+		return err
+	}
+	l := lexer.New(string(content))
+	p := parser.New(l)
+	program := p.ParseProgram()
+	if len(p.Errors()) != 0 {
+		return errors.New("parser errors:\n\t" + strings.Join(p.Errors(), "\n\t"))
+	}
+	env := object.NewEnvironment()
+	evaluated := eval.Eval(program, env)
+	fmt.Println(evaluated.Inspect())
 	return nil
 }
