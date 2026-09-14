287d504b9f3b957cf264d29fc7e3ed27d3963f63
Shihab Mahamud <shihab4t@gmail.com>
2026-09-15 02:28:11 +0600

    run source

--- main.go
+++ main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"monkey/repl"
+	"monkey/source"
+	"os"
 )
 
 func main() {
+	if len(os.Args) == 2 {
+		err := source.Run()
+		if err != nil {
+			panic(err)
+		}
+		return
+	}
 	err := repl.Run()
 	if err != nil {
 		panic(err)
--- source/source.go
+++ source/source.go
@@ -1,1 +1,12 @@
 package source
+
+import (
+	"fmt"
+	"os"
+)
+
+func Run() error {
+	filename := os.Args[1]
+	fmt.Printf("%+v\n", filename)
+	return nil
+}
