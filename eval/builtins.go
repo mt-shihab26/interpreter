e60e1a34934287d6642700703476b905613c5daf
Shihab Mahamud <shihab4t@gmail.com>
2026-09-17 00:06:30 +0600

    array push

--- eval/builtins.go
+++ eval/builtins.go
@@ -69,4 +69,22 @@ var builtins = map[string]*object.Builtin{
 			}
 		},
 	},
+	"push": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 2 {
+				return newErrorObject("wrong number of arguments. got=%d, want=2",
+					len(args))
+			}
+			if args[0].Type() != object.ARRAY {
+				return newErrorObject("argument to `push` must be ARRAY, got %s",
+					args[0].Type())
+			}
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+			newElements := make([]object.Object, length+1)
+			copy(newElements, arr.Elements)
+			newElements[length] = args[1]
+			return &object.Array{Elements: newElements}
+		},
+	},
 }
